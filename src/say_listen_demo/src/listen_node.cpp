bbdd4d457da4ff273429bd455c9609c819b49470
Antonio <dongyixiao2001@163.com>
2022-08-29 23:51:53 +0800

    Merge say, listen package

--- src/say_listen_demo/src/listen_node.cpp
+++ src/say_listen_demo/src/listen_node.cpp
@@ -6,11 +6,15 @@
 void CallBack(const std_msgs::String::ConstPtr &msg){
     ROS_INFO("I Heard %s",msg->data.c_str());
 }
+void CallBack_new(const std_msgs::String::ConstPtr &msg){
+    ROS_INFO("I Heard_new %s",msg->data.c_str());
+}
 
 int main(int argc, char ** argv){
     ros::init(argc,argv, "listener");
     ros::NodeHandle n;
     ros::Subscriber sub = n.subscribe("say_topic",10,CallBack); //(接收来自topic,队列长度，回调函数)
+    ros::Subscriber sub_new = n.subscribe("say_topic_new",10,CallBack_new); //(接收来自topic,队列长度，回调函数)
     ros::spin(); //反复查看队列，处理、清空队列；反复调用当前可触发的回调函数
     return 0;
 }
--- src/say_listen_demo/src/say_node.cpp
+++ src/say_listen_demo/src/say_node.cpp
@@ -7,17 +7,19 @@ int main(int argc, char ** argv){
     ros::init(argc,argv, "say_talker"); //解析参数，命名当前node
     ros::NodeHandle n;  //创建句柄，实例化node
     ros::Publisher say_pub = n.advertise<std_msgs::String>("say_topic",10); //(发送的目标topic，消息队列长度)
-    ros::Publisher say_pub_new = n.advertise<msgs::Msg>("say_topic_new",10);
-    ros::Rate loop_rate(10);    //控制rate=10.0Hz
-    int count=0;
+    // ros::Publisher say_pub_new = n.advertise<msgs::Msg>("say_topic_new",10);
+    ros::Publisher say_pub_new = n.advertise<std_msgs::String>("say_topic_new",10);
+    ros::Rate loop_rate(1);    //控制rate=16.0Hz
+    int count = 0;
 
     while(ros::ok()){
         std_msgs::String msg;
         std::stringstream ss;
-        ss << "Hello say" << count;
+        ss << "Hello say " << count;
         count++;
         msg.data = ss.str();
         say_pub.publish(msg);
+        say_pub_new.publish(msg);
 
         //参数中心：传递外界读入数据
         std::string param_string;
